72ffea0d7d0811d4e6597ae68733a71d982d1ae5
TomBenzo <98659510+TomBenzo@users.noreply.github.com>
2022-04-05 01:54:46 -0400

    Delete app directory

--- app/shop/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, redirect, render_template, request, url_for
-from flask_login import current_user, login_required
-
-
-shop = Blueprint('shop', __name__, template_folder='shop_templates')
-
-from app.models import db, Product
-
-
